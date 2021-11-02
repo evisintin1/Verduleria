3242f31dd477d58fca6ee185a21ef4977c94aa0a
evisintin1 <you@example.com>
2021-11-02 01:06:09 -0300

    Puerto

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const express = require('express');
+const { Console } = require('console');
+const app = express();
+
+const hostname = '127.0.0.1'
+const port = 2500
+
+app.listen(port,()=>{
+    console.log("Servidor corriendo")
+})
+
+app.get('/puerto', function(req, res)  {
+    res.send('Servidor corriendo en el puerto:' + port)
+  });
