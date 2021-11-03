c008217cf637fa71e51d3da5986367ab53bd2ed7
evisintin1 <gtaborda@escuelasproa.edu.ar>
2021-11-03 09:50:09 -0300

    "solucion"

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { Console } = require('console');
 const app = express();
 
-const hostname = '127.0.0.1'
-const port = 2500
+const hostname = '127.0.0.1';
+const port = 2500;
+let verdura;
+app.use(express.json);
 
 app.listen(port,()=>{
-    console.log("Servidor corriendo")
+    console.log("Servidor corriendo");
 })
 
 app.get('/puerto', (req, res) => {
@@ -16,12 +18,14 @@ app.get('/puerto', (req, res) => {
 
 
 app.post('/verdura',(req,res)=>{
-    let suma=0;
-    for (var i in req.body) {
-        if(req.body[i].tipo=='Verdura'){
-            suma+req.body[i].precio
+    verdura=req.body;
+    for (let i in verdura) {
+        if(verdura[i].tipo=='Verdura'){
+            
+            suma += verdura[i].precio;
         }
         
     }
+    res.send('El valor total es' + suma);
 
 })
